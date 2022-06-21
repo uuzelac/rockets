af94cb1653ae989663ea13059d4e8ac24ef2a224
Uroš Uzelac <uros.uzelac@guidefitter.com>
2022-06-21 14:48:23 +0200

    theme colors added

---
+++ src/theme/colors.ts
@@ -0,0 +1,7 @@
+export const white = '#FFFFFF';
+export const grey = '#BEBEBE';
+export const greyDark = '#5A5A5A';
+export const black = '#3A3A3A';
+export const yellow = '#F9E881';
+export const red = '#E05E45';
+export const green = '#97E05B';
