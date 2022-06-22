f9bdbc41f19716cf5834daae2f558e78b38255b6
Uroš Uzelac <uros.uzelac@guidefitter.com>
2022-06-22 23:56:28 +0200

    added selector for single crew member

--- src/redux/crewMembers/selectors.ts
+++ src/redux/crewMembers/selectors.ts
@@ -1,5 +1,11 @@
 import CrewMember from './models/CrewMember';
 import { RootReducerType } from '../../redux/types';
 
-export const getAllCrewMembers = (state: RootReducerType): null | CrewMember[] => state.crewMembers.item;
-export const isLoadingCrewMembers = (state: RootReducerType): boolean => state.crewMembers.isLoading;
+export const getAllCrewMembers = (state: RootReducerType): null | CrewMember[] =>
+  state.crewMembers.item;
+
+export const isLoadingCrewMembers = (state: RootReducerType): boolean =>
+  state.crewMembers.isLoading;
+
+export const getCrewMemberById = (state: RootReducerType, id: string): CrewMember | undefined =>
+  state.crewMembers.item?.find((member) => member.id === id);
