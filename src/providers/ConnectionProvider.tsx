a2c47af3d476e02c1ab345f33d17e41d7ba4015c
Uroš Uzelac <uros.uzelac@guidefitter.com>
2022-06-22 18:08:50 +0200

    ConnectionProvider updated

--- src/providers/ConnectionProvider.tsx
+++ src/providers/ConnectionProvider.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import NetInfo from '@react-native-community/netinfo';
+import Toast from 'react-native-toast-message';
 import { useDispatch } from 'react-redux';
 import { setConnection } from '../redux/appState/reducers';
 
@@ -12,6 +13,13 @@ const ConnectionProvider = ({ children }: ConnectionProviderProps): JSX.Element 
   useEffect(() => {
     const unsubscribe = NetInfo.addEventListener((state): void => {
         dispatch(setConnection(state.isConnected));
+        if(!state.isConnected) {
+          Toast.show({
+            type: 'error',
+            text1: 'No internet connection',
+            text2: 'An error occurred due to connectivity issues.'
+          });
+        }
     });
 
     return unsubscribe();
